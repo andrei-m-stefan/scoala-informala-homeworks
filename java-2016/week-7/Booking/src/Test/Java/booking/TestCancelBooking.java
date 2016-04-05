d06302864ec61112f42ff6d7a9121a187b3c62b8
Andrei <andrei.m.stefan@hotmail.com>
2016-04-05 18:58:31 +0300

    +added TestCancelBooking class.

---
+++ java-2016/week-7/Booking/src/Test/Java/booking/TestCancelBooking.java
@@ -0,0 +1,113 @@
+package booking;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestCancelBooking {
+
+	List<Accommodation> accomodations;
+
+	List<BookingPeriod> periods;
+
+	List<Booking> bookings;
+
+	@Before
+	public void init() {
+
+		Date from = getDate(2016, 5, 1);
+		Date to = getDate(2016, 8, 27);
+		
+		Season season = createSeason(SeasonType.HIGH, from, to);
+		RoomFair roomFair = createRoomFair(100, season);
+		Accommodation accomodation = createNewRoom(AccommodationType.ROYAL, roomFair);
+		
+		accomodations = new ArrayList<Accommodation>();
+		
+		accomodations.add(accomodation);
+		
+		periods = new ArrayList<BookingPeriod>();
+		
+		periods.add(new BookingPeriod(getDate(2016, 5, 12), getDate(2016, 5, 17)));
+		periods.add(new BookingPeriod(getDate(2016, 6, 10), getDate(2016, 6, 13)));
+		periods.add(new BookingPeriod(getDate(2016, 7, 5), getDate(2016, 7, 12)));
+		periods.add(new BookingPeriod(getDate(2016, 8, 2), getDate(2016, 8, 4)));
+
+		bookings = new ArrayList<Booking>();
+		
+		bookings.add(new Booking(accomodation, periods.get(0)));
+		bookings.add(new Booking(accomodation, periods.get(1)));
+		bookings.add(new Booking(accomodation, periods.get(2)));
+		bookings.add(new Booking(accomodation, periods.get(3)));
+	}
+
+	@After
+	public void destroy() {
+		accomodations = null;
+		periods = null;
+		bookings = null;
+	}
+	
+	@Test 
+	public void testCancelBooking() {
+		Date from = getDate(2016, 5, 11);
+		Date to = getDate(2016, 5, 18);
+		cancelBooking(from, to);
+		assertEquals(3, bookings.size());
+	}
+	
+	private boolean cancelBooking(Date from, Date to) {
+		boolean isBooked = false;
+		int index = 0;
+		for (Booking booking : bookings) {
+			isBooked = booking.getBookingPeriod().getFrom().after(from) && booking.getBookingPeriod().getTo().before(to);
+			index++;
+			if (isBooked) {
+				bookings.remove(index-1);
+				System.out.println("Booking canceled.");
+				break;
+			}
+		}
+		return isBooked;
+	}
+	
+	
+	
+	private Accommodation createNewRoom(AccommodationType type, RoomFair roomFair) {
+		Accommodation accomodation = new Accommodation();
+		accomodation.setType(type);
+		accomodation.setFair(roomFair);
+		return accomodation;
+	}
+
+	private RoomFair createRoomFair(int i, Season season) {
+		RoomFair roomFair = new RoomFair();
+		roomFair.setSeason(season);
+		roomFair.setValue(i);
+		return roomFair;
+	}
+
+	private Season createSeason(SeasonType type, Date from, Date to) {
+		Season season = new Season();
+		season.setFrom(from);
+		season.setTo(to);
+		season.setType(type);
+		return season;
+	}
+
+	private Date getDate(int year, int month, int day) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.YEAR, year);
+		calendar.set(Calendar.MONTH, month - 1);
+		calendar.set(Calendar.DAY_OF_MONTH, day);
+
+		return calendar.getTime();
+	}
+}
